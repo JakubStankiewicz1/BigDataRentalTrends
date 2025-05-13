55307e318d2d6add26aaf509b209aea31239d08b
Kuba <272264@student.pwr.edu.pl>
2025-05-13 16:14:13 +0200

    Fixed otodom model

--- main_otodom.py
+++ main_otodom.py
@@ -48,78 +48,30 @@ def parse_listing(url):
     data["title"] = h1.get_text(strip=True) if h1 else None
 
     # Price
-    price_wr = soup.select_one("div[data-sentry-element='MainPricewrapper']")
-    data["price"] = price_wr.get_text(strip=True) if price_wr else None
-    fee_wr = soup.select_one("div[data-sentry-element='AdditionalPriceWrapper']")
-    data["rent_fee"] = fee_wr.get_text(strip=True) if fee_wr else None
+    price_el = soup.select_one("strong[data-cy='adPageHeaderPrice']")
+    data["price"] = price_el.get_text(strip=True) if price_el else None
+
+    # Rent Fee (Additional Price)
+    fee_el = soup.select_one("div[data-sentry-element='AdditionalPriceWrapper']")
+    data["rent_fee"] = fee_el.get_text(strip=True) if fee_el else None
 
     # Location
-    loc = soup.select_one("div[data-sentry-element='MapLink'] + div p")
-    data["location"] = loc.get_text(strip=True) if loc else None
+    loc_el = soup.select_one("div[data-sentry-element='Container'] a[data-sentry-element='StyledLink']")
+    data["location"] = loc_el.get_text(strip=True) if loc_el else None
 
-    # Detail items (grid)
-    params = {
-        "rooms": None, "area": None, "floor": None, "heating": None,
-        "condition": None, "available_from": None, "deposit": None,
-        "advertiser_type": None
-    }
-    for item in soup.select("div.css-1xw0jqp.esen0m91"):
-        lbl = item.select_one("p.css-1airkmu")
-        val = item.select("p.css-1airkmu")
-        if not lbl or len(val)<2: continue
-        key, value = lbl.get_text(strip=True).lower(), val[1].get_text(strip=True)
-        if "liczba pokoi" in key:       params["rooms"] = value
-        elif "powierzchnia" in key:     params["area"] = value
-        elif "piętro" in key:           params["floor"] = value
-        elif "ogrzewanie" in key:       params["heating"] = value
-        elif "stan wykończenia" in key: params["condition"] = value
-        elif "dostępne od" in key:      params["available_from"] = value
-        elif "czynsz" in key:           params["rent_fee"] = value  # override if needed
-        elif "kaucja" in key:           params["deposit"] = value
-        elif "typ ogłoszeniodawcy" in key: params["advertiser_type"] = value
+    # Number of Rooms
+    rooms_el = soup.select_one("div[data-sentry-element='ItemGridContainer'] p:-soup-contains('Liczba pokoi:') + p")
+    data["rooms"] = rooms_el.get_text(strip=True) if rooms_el else None
 
-    data.update(params)
+    # Advertiser Type
+    advertiser_el = soup.select_one("div[data-sentry-element='ItemGridContainer'] p:-soup-contains('Typ ogłoszeniodawcy:') + p")
+    data["advertiser_type"] = advertiser_el.get_text(strip=True) if advertiser_el else None
 
-    # Extras (garaż/piwnica etc)
-    extras_el = soup.select_one("div.css-1xw0jqp.esen0m91 p span.css-axw7ok")
-    if extras_el:
-        data["extras"] = ", ".join(sp.get_text(strip=True) for sp in 
-                                   soup.select("div.css-1xw0jqp.esen0m91 p span.css-axw7ok"))
-    else:
-        data["extras"] = None
-
-    # Building & Materials / Elevator / Security
-    extras2 = {"building_type": None, "elevator": None, "security": None}
-    # these appear under the accordion first section
-    for block in soup.select("button.css-12wre93 + div.css-t6wh4q div.css-1xw0jqp"):
-        lbl = block.select_one("p.css-1airkmu")
-        val = block.select_one("p.css-1airkmu + p")
-        if not lbl or not val: continue
-        key, value = lbl.get_text(strip=True).lower(), val.get_text(strip=True)
-        if "rodzaj zabudowy" in key:    extras2["building_type"] = value
-        elif "winda" in key:            extras2["elevator"] = value
-        elif "bezpieczeństwo" in key:   extras2["security"] = value
-
-    data.update(extras2)
-
-    # Equipment & Media (second accordion)
-    equip = soup.select("div.css-1svn882 p.css-1airkmu span.css-axw7ok")
-    if equip:
-        # group media vs equipment by header context
-        # for simplicity lump all into one comma list
-        data["equipment_media"] = ", ".join(sp.get_text(strip=True) for sp in equip)
-    else:
-        data["equipment_media"] = None
-
-    # Description (full accordion)
-    desc = soup.select_one("div[data-cy='adPageAdDescription']")
-    data["description"] = desc.get_text(" ", strip=True) if desc else None
-
-    # ID
-    id_tag = soup.find(string=lambda t: isinstance(t,str) and "ID:" in t)
+    # ID (unchanged)
+    id_tag = soup.find(string=lambda t: isinstance(t, str) and "ID:" in t)
     data["id"] = id_tag.split("ID:")[-1].strip() if id_tag else url.rstrip("/").split("-")[-1]
 
-    # URL & timestamp
+    # URL & timestamp (unchanged)
     data["url"] = url
     data["scrape_date"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     return data
